695cb78a7b39d7b2f22ce23ac0512c3ab445e911
Fatih Eren Özçelik <eren.ozcelik@std.antalya.edu.tr>
2021-07-27 17:27:48 +0300

    27.07.2021

--- EntityLayer/Concrete/Content.cs
+++ EntityLayer/Concrete/Content.cs
@@ -24,7 +24,7 @@ namespace EntityLayer.Concrete
         public virtual Heading Heading { get; set; }
 
 
-        public int WriterID { get; set; }
+        public int? WriterID { get; set; }
         public virtual Writer Writer { get; set; }
     }
 }
